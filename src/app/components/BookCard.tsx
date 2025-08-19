91b3c1be264d7fdfddf932797d3559f22940ec06
HieuNgo12 <cusaniv@gmail.com>
2025-08-19 17:19:26 +0700

    fix
    gas

---
+++ src/app/components/BookCard.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Image from 'next/image';
+import BasicSelect from './Select';
+
+const bull = (
+    <Box
+        component="span"
+        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+        •
+    </Box>
+);
+
+export default function BasicCard() {
+    return (
+        <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+                <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+                    Book's Title
+                </Typography>
+                <Typography variant="h5" component="div">
+                    <Image
+                        src="/file.svg"
+                        width={20}
+                        height={20}
+                        alt="Picture of the author"
+                    />
+                </Typography>
+                <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
+                <Typography variant="body2">
+                    Book's description
+                    <br />
+                    {'"a benevolent smile"'}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                
+                <BasicSelect />
+            </CardActions>
+        </Card>
+    );
+}
